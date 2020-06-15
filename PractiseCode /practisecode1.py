6bee619c0585ff2f0ce372306abce02f1d82ce94
thedizzyprogrammer <akprogramming@outlook.com>
2020-06-15 19:14:33 +0100

    Added code example 3

--- PractiseCode /practisecode1.py
+++ PractiseCode /practisecode1.py
@@ -1,3 +1,5 @@
+#devoloped by thedizzyprogrammer
+
 import matplotlib.pyplot as plt 
 
 plt.plot([1,2,3],[5,7,20])
--- PractiseCode /practisecode3.py
+++ PractiseCode /practisecode3.py
@@ -1,20 +1,19 @@
 #devoloped by thedizzyprogrammer
 
-
 import matplotlib.pyplot as plt 
 
-x = [1,2,3]
+x = [1,2,3,4,5,6,7,8,9,10]
 
-y = [5,7,20]
+y = [1,2,3,4,5,6,7,8,9,10]
 
 x2 = [1,2,3]
 
 y2 = [20,21,29]
 
 
-plt.plot(x, y, label='First Line')
+plt.bar(x2,y2, label='Bar Chart Example Number 2', color='grey')
 
-plt.plot(x2,y2, label='Second Line')
+plt.bar(x, y, label='Bar Chart Example Number 1')
 
 plt.xlabel ('Var1')
 
