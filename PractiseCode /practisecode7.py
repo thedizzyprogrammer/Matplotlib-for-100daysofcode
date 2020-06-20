83befb9a170bcb15840d9f11796458098cdd44c9
thedizzyprogrammer <akprogramming@outlook.com>
2020-06-20 18:07:48 +0100

    Created 3D graphs

---
+++ PractiseCode /practisecode7.py
@@ -0,0 +1,13 @@
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt  
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111, projection='3d')
+
+x = [1,2,3,4,5,6,7,8,9,10]
+y = [5,6,7,8,2,5,6,3,7,2]
+z = [1,2,6,3,2,7,3,3,7,2]
+
+ax1.scatter(x, y, z)
+
+plt.show()
